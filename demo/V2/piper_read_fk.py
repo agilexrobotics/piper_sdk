8ec85eeffef09305c86d0b56938e4b79083aed42
HoranTian <1915168688@qq.com>
2025-02-10 15:09:16 +0800

    修个Getfk demo

--- demo/V2/piper_read_fk.py
+++ demo/V2/piper_read_fk.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     piper = C_PiperInterface_V2()
     piper.ConnectPort()
     while True:
-        print(piper.GetFK())
-        print(piper.GetArmEndPoseMsgs())
-        # print(piper.GetArmJointMsgs())
+        print(f"feedback:{piper.GetFK('feedback')}")
+        print(f"control:{piper.GetFK('control')}")
         time.sleep(0.01)
