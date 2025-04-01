4eddfcf817cd87de9acee316a72cf5b988025378
RosenYin <2356838399@qq.com>
2025-04-01 19:10:13 +0800

    [FEAT]增加机械臂限位参数设定demo,修改CHANGELOG

---
+++ demo/V1/piper_sdk_param.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*-coding:utf8-*-
+# 注意demo无法直接运行，需要pip安装sdk后才能运行
+from typing import (
+    Optional,
+)
+import time
+from piper_sdk import *
+
+if __name__ == "__main__":
+    piper = C_PiperInterface("can0")
+    piper.ConnectPort()
+    print(piper.GetSDKJointLimitParam('j6'))
+    print(piper.GetSDKGripperRangeParam())
+    piper.SetSDKGripperRangeParam(0, 0.05)
+    piper.SetSDKJointLimitParam('j6',-2.09, 2.09)
+    print(piper.GetSDKJointLimitParam('j6'))
+    print(piper.GetSDKGripperRangeParam())
