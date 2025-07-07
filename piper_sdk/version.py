5a6f75d3d871ac9899e8810307fd66a893764c3b
RosenYin <2356838399@qq.com>
2025-07-07 15:26:21 +0800

    📃 docs(说明文档): 增加0.3.2版本的说明

--- piper_sdk/version.py
+++ piper_sdk/version.py
@@ -7,7 +7,8 @@ class PiperSDKVersion(Enum):
     PIPER_SDK_VERSION_0_2_19 = '0.2.19'
     PIPER_SDK_VERSION_0_2_20 = '0.2.20'
     PIPER_SDK_VERSION_0_3_0 = '0.3.0'
-    PIPER_SDK_CURRENT_VERSION = PIPER_SDK_VERSION_0_3_0
+    PIPER_SDK_VERSION_0_3_2 = '0.3.2'
+    PIPER_SDK_CURRENT_VERSION = PIPER_SDK_VERSION_0_3_2
     PIPER_SDK_VERSION_UNKNOWN = 'unknown'
     def __str__(self):
         return f"{self.name} ({self.value})"
