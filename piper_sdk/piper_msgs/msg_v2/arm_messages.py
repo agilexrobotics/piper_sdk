f1240ca283656dfd4b321c40a5a2e09a57ceeaa1
RosenYin <2356838399@qq.com>
2025-08-11 15:31:35 +0800

    🦄 refactor(msg): 修改msg的init结构

--- piper_sdk/piper_msgs/msg_v2/arm_messages.py
+++ piper_sdk/piper_msgs/msg_v2/arm_messages.py
@@ -11,41 +11,11 @@ from typing import (
     Optional,
 )
 from .arm_msg_type import ArmMsgType
-from .feedback.arm_feedback_end_pose import ArmMsgFeedBackEndPose
-from .feedback.arm_feedback_joint_states import ArmMsgFeedBackJointStates
-from .feedback.arm_feedback_status import ArmMsgFeedbackStatus
-from .feedback.arm_feedback_gripper import ArmMsgFeedBackGripper
-from .feedback.arm_feedback_current_end_vel_acc_param import ArmMsgFeedbackCurrentEndVelAccParam
-from .feedback.arm_feedback_current_motor_angle_limit_max_spd import ArmMsgFeedbackCurrentMotorAngleLimitMaxSpd, ArmMsgFeedbackAllCurrentMotorAngleLimitMaxSpd
-from .feedback.arm_feedback_current_motor_max_acc_limit import ArmMsgFeedbackCurrentMotorMaxAccLimit, ArmMsgFeedbackAllCurrentMotorMaxAccLimit
-from .feedback.arm_feedback_joint_vel_acc import ArmMsgFeedbackJointVelAcc, ArmMsgFeedbackAllJointVelAcc
-from .feedback.arm_feedback_high_spd import ArmMsgFeedbackHighSpd
-from .feedback.arm_feedback_low_spd import ArmMsgFeedbackLowSpd
-#----------------------------------基于V1.5-2版本后---------------------------------------------#
-from .feedback.arm_feedback_gripper_teaching_param import ArmMsgFeedbackGripperTeachingPendantParam
-#---------------------------------------------------------------------------------------------#
+# 导入 feedback 子模块的类
+from .feedback import *
+# 导入 transmit 子模块的类
+from .transmit import *
 
-from .transmit.arm_motion_ctrl_1 import ArmMsgMotionCtrl_1
-from .transmit.arm_motion_ctrl_2 import ArmMsgMotionCtrl_2
-from .transmit.arm_motion_ctrl_cartesian import ArmMsgMotionCtrlCartesian
-from .transmit.arm_joint_ctrl import ArmMsgJointCtrl
-from .transmit.arm_circular_pattern import ArmMsgCircularPatternCoordNumUpdateCtrl
-from .transmit.arm_gripper_ctrl import ArmMsgGripperCtrl
-from .transmit.arm_master_slave_config import ArmMsgMasterSlaveModeConfig
-from .transmit.arm_motor_enable_disable import ArmMsgMotorEnableDisableConfig
-from .transmit.arm_search_motor_max_angle_spd_acc_limit import ArmMsgSearchMotorMaxAngleSpdAccLimit
-from .transmit.arm_motor_angle_limit_max_spd_config import ArmMsgMotorAngleLimitMaxSpdSet
-from .transmit.arm_joint_config import ArmMsgJointConfig
-from .transmit.arm_set_instruction_response import ArmMsgInstructionResponseConfig
-from .transmit.arm_param_enquiry_and_config import ArmMsgParamEnquiryAndConfig
-from .transmit.arm_end_vel_acc_param_config import ArmMsgEndVelAccParamConfig
-from .transmit.arm_crash_protection_rating_config import ArmMsgCrashProtectionRatingConfig
-from .feedback.arm_feedback_crash_protection_rating import ArmMsgFeedbackCrashProtectionRating
-#----------------------------------基于V1.5-2版本后---------------------------------------------#
-from .transmit.arm_joint_mit_ctrl import ArmMsgJointMitCtrl
-from .transmit.arm_joint_mit_ctrl import ArmMsgAllJointMitCtrl
-from .transmit.arm_gripper_teaching_param_config import ArmMsgGripperTeachingPendantParamConfig
-#---------------------------------------------------------------------------------------------#
 class PiperMessage:
     '''
     msg_v2
--- piper_sdk/piper_msgs/msg_v2/transmit/__init__.py
+++ piper_sdk/piper_msgs/msg_v2/transmit/__init__.py
@@ -1,1 +1,46 @@
 
+# 导入 transmit 子模块
+from .arm_circular_pattern import ArmMsgCircularPatternCoordNumUpdateCtrl
+from .arm_crash_protection_rating_config import ArmMsgCrashProtectionRatingConfig
+from .arm_end_vel_acc_param_config import ArmMsgEndVelAccParamConfig
+from .arm_gripper_ctrl import ArmMsgGripperCtrl
+#----------------------------------基于V1.5-2版本后---------------------------------------------#
+from .arm_gripper_teaching_param_config import ArmMsgGripperTeachingPendantParamConfig
+#---------------------------------------------------------------------------------------------#
+from .arm_joint_config import ArmMsgJointConfig
+from .arm_joint_ctrl import ArmMsgJointCtrl
+#----------------------------------基于V1.5-2版本后---------------------------------------------#
+from .arm_joint_mit_ctrl import ArmMsgJointMitCtrl
+from .arm_joint_mit_ctrl import ArmMsgAllJointMitCtrl
+#---------------------------------------------------------------------------------------------#
+from .arm_master_slave_config import ArmMsgMasterSlaveModeConfig
+from .arm_motion_ctrl_1 import ArmMsgMotionCtrl_1
+from .arm_motion_ctrl_2 import ArmMsgMotionCtrl_2
+from .arm_motion_ctrl_cartesian import ArmMsgMotionCtrlCartesian
+from .arm_motor_angle_limit_max_spd_config import ArmMsgMotorAngleLimitMaxSpdSet
+from .arm_motor_enable_disable import ArmMsgMotorEnableDisableConfig
+from .arm_param_enquiry_and_config import ArmMsgParamEnquiryAndConfig
+from .arm_search_motor_max_angle_spd_acc_limit import ArmMsgSearchMotorMaxAngleSpdAccLimit
+from .arm_set_instruction_response import ArmMsgInstructionResponseConfig
+
+__all__ = [
+    # 发送
+    'ArmMsgMotionCtrl_1',
+    'ArmMsgMotionCtrl_2',
+    'ArmMsgMotionCtrlCartesian',
+    'ArmMsgJointCtrl',
+    'ArmMsgCircularPatternCoordNumUpdateCtrl',
+    'ArmMsgGripperCtrl',
+    'ArmMsgMasterSlaveModeConfig',
+    'ArmMsgMotorEnableDisableConfig',
+    'ArmMsgSearchMotorMaxAngleSpdAccLimit',
+    'ArmMsgMotorAngleLimitMaxSpdSet',
+    'ArmMsgJointConfig',
+    'ArmMsgInstructionResponseConfig',
+    'ArmMsgParamEnquiryAndConfig',
+    'ArmMsgEndVelAccParamConfig',
+    'ArmMsgCrashProtectionRatingConfig',
+    'ArmMsgGripperTeachingPendantParamConfig',
+    'ArmMsgJointMitCtrl',
+    'ArmMsgAllJointMitCtrl'
+]
